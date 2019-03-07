eb3e45e62e021d25f3bace306470d4dea164c9c4
Henrique Caúla <lhtc@cin.ufpe.br>
2019-03-07 08:52:05 -0300

    Adds twitter mentions regex

--- server.rb
+++ server.rb
@@ -12,8 +12,17 @@ client = Twitter::REST::Client.new do |config|
     config.access_token_secret = settings.access_token_secret
 end
 
-get "/search/" do
-    client.user_timeline("le_santti", :count => 200).collect do |tweet|
-        "#{tweet.user.screen_name}: #{tweet.text}\n\n"
-    end
+def get_mentions(tweet)
+    return tweet.scan(/.*?(@\w+).*?/)
+end
+
+def retrieve_mentions(user)
+    # client.user_timeline("le_santti", :count => 200).collect do |tweet|
+    #     "#{tweet.user.screen_name}: #{tweet.text}\n\n"
+    # end
+end
+
+get "/layer" do
+    # users = params[:users].split ","
+    puts get_mentions("@onllayne Carina saiu no lucro, nem vai ter @jardimderecife que ir no shopping metrópole e ainda pegou o namorado da Cássia")
 end
